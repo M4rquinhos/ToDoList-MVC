440e0dd2d47d03238953530cd52d258f8d3b7eaf
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-02-03 15:37:54 -0600

    Agregar archivos de proyecto.

---
+++ ToDoList/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models.Entidades;
+
+namespace ToDoList.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+    }
+}
