5323da2b656b730d09a1efe30a643dc68f381ea2
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-02-03 23:29:14 -0600

    empezando a usar vistas

---
+++ ToDoList/Models/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campos debe ser un correo electronico valido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
